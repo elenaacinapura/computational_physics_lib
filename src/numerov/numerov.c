520b7c487d73ef0dd6728d7fb3af757c2f10dc26
elenaacinapura <elena.acinapura@gmail.com>
2021-04-05 11:55:32 +0200

    Changes to numerov

--- src/numerov/numerov.c
+++ src/numerov/numerov.c
@@ -1,6 +1,6 @@
 #include <numerov.h>
 #include <complex.h>
 
-void numerov_1D (double x, complex double psi_curr, complex double psi_prec, double dx, double F (double, void *), void *param) {
+complex double numerov_1D (double x, complex double psi_curr, complex double psi_prec, double dx, double F (double, void *), void *param) {
     return (psi_curr * (2.0 + 5.0/6 * dx*dx * F(x, param)) - psi_prec * (1 - dx*dx / 12.0 * F(x - dx, param)))/(1 - dx*dx/12.0 * F(x + dx, param));
 }
