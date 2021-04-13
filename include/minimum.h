c952421fc13fccab3364b182a3fe81a849aa9f65
elenaacinapura <elena.acinapura@gmail.com>
2021-04-13 10:41:32 +0200

    assertion and documentation

--- include/minimum.h
+++ include/minimum.h
@@ -1,6 +1,17 @@
 #ifndef __MINIMUM_H__
 #define __MINIMUM_H__
-
-double minimum (double f (double, void *), double low, double high, void *p);
+/**
+ * @brief Find minimum of function f between given.
+ *
+ * Calculate the point where the function f has minimum, restricting the rearch between the given bounds.
+ *
+ * @param f Function to find minimum of.
+ * @param low Lower bound for the search of the minimum.
+ * @param high Higher bound for the search of the minimum.
+ * @param p Pointer to the parameters struct of function f.
+ *
+ * @return The point where the function f has minimum.
+ */
+double minimum(double f(double, void *), double low, double high, void *p);
 
 #endif
--- src/minimum/minimum.c
+++ src/minimum/minimum.c
@@ -1,22 +1,23 @@
-#include <minimum.h>
+#include <assert.h>
 #include <math.h>
+#include <minimum.h>
 #include <stdio.h>
 
-double minimum (double f (double, void *), double low, double high, void *p) {
-    
-
-    double x1, x2, x3, x4;
-    x1 = low;
-    x4 = high;
-    while (fabs((x4-x1)/(x4+x1)) > EPS) {
-        x2 = x1  + (x4 - x1)/4.0;
-        x3 = x2 + (x4 - x1)/2.0;
-        if (f(x2, p) < f(x3, p)){
-            x4 = x3;
-        } else {
-            x1 = x2;
-        }
-    }
-
-    return x1;
+double minimum(double f(double, void *), double low, double high, void *p) {
+	assert(low <= high);
+	assert(isfinite(low));
+	assert(isfinite(high));
+	double x1, x2, x3, x4;
+	x1 = low;
+	x4 = high;
+	while (fabs((x4 - x1) / (x4 + x1)) > EPS) {
+		x2 = x1 + (x4 - x1) / 4.0;
+		x3 = x2 + (x4 - x1) / 2.0;
+		if (f(x2, p) < f(x3, p)) {
+			x4 = x3;
+		} else {
+			x1 = x2;
+		}
+	}
+	return x1;
 }
