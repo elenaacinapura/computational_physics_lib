e3f13f346e721aef5ee59860d14fbe0284c20994
elenaacinapura <elena.acinapura@gmail.com>
2021-04-18 15:32:18 +0200

    zero bisection correction

--- src/numerical_methods/zero_bisection.c
+++ src/numerical_methods/zero_bisection.c
@@ -1,22 +1,29 @@
-#include <numerical_methods/zero_bisection.h>
 #include <assert.h>
 #include <math.h>
+#include <numerical_methods/zero_bisection.h>
 
-double zero_bisection(double f (double, void *), double low, double high, void *param){
-    assert(low < high);
-    assert(isfinite(low) && isfinite(high));
-    assert(f(low, param) * f(high, param) < 0.0);
+double zero_bisection(double f(double, void *), double low, double high, void *param) {
+	assert(low < high);
+	assert(isfinite(low) && isfinite(high));
+	assert(f(low, param) * f(high, param) <= 0.0);
 
-    while (fabs(high - low)/(fabs(high + low) > EPS)) {
-        double mid = (high + low) / 2.0;
-        if (f(mid, param) == 0.0) {
-            return mid;
-        }
-        if (f(low, param) * f(mid, param) < 0.0) {
-            high = mid;
-        } else {
-            low = mid;
-        }
-    }
-    return low;
+	if (f(low, param) == 0.0) {
+		return low;
+	}
+    if (f(low, param) == 0.0) {
+		return high;
+	}
+
+	while (fabs(high - low) / (fabs(high + low) > EPS)) {
+		double mid = (high + low) / 2.0;
+		if (f(mid, param) == 0.0) {
+			return mid;
+		}
+		if (f(low, param) * f(mid, param) < 0.0) {
+			high = mid;
+		} else {
+			low = mid;
+		}
+	}
+	return low;
 }
