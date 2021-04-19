55ae3bd51f501a5eacb6e07fac3ff6fb7debf2fa
elenaacinapura <elena.acinapura@gmail.com>
2021-04-19 09:36:48 +0200

    better parameters name in blas wrappers

--- include/linear_algebra/blas_wrappers.h
+++ include/linear_algebra/blas_wrappers.h
@@ -5,9 +5,9 @@
 
 void vec_copy (int N, double src[], double dest[]);
 
-void mat_vec_mul (int m, int n, double *M, double *v, double *res);
+void mat_vec_mul(int nrows, int ncols, double *M, double *v, double *res);
 
-void mat_mat_mul(int m, int n, int k, double *M1, double *M2, double *res);
+void mat_mat_mul(int nrows1, int ncols2, int ncols1_nrows2, double *M1, double *M2, double *res);
 
 
 #endif
--- src/linear_algebra/blas_wrappers.c
+++ src/linear_algebra/blas_wrappers.c
@@ -7,12 +7,12 @@ void vec_copy (int N, double src[], double dest[]){
 	cblas_dcopy(N, (double *)src, 1, (double *)dest, 1);
 }
 
-void mat_vec_mul(int m, int n, double *M, double *v, double *res) {
-	assert(m > 0 && n > 0);
-	cblas_dgemv(CblasRowMajor, CblasNoTrans, m, n, 1, M, n, v, 1, 0, res, 1);
+void mat_vec_mul(int nrows, int ncols, double *M, double *v, double *res) {
+	assert(nrows > 0 && ncols > 0);
+	cblas_dgemv(CblasRowMajor, CblasNoTrans, nrows, ncols, 1, M, ncols, v, 1, 0, res, 1);
 }
 
-void mat_mat_mul(int m, int n, int k, double *M1, double *M2, double *res) {
-	assert(m > 0 && n > 0 && k > 0);
-	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, M1, m, M2, k, 0, res, m);
+void mat_mat_mul(int nrows1, int ncols2, int ncols1_nrows2, double *M1, double *M2, double *res) {
+	assert(nrows1 > 0 && ncols2 > 0 && ncols1_nrows2 > 0);
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, nrows1, ncols2, ncols1_nrows2, 1, M1, nrows1, M2, ncols1_nrows2, 0, res, nrows1);
 }
