01e5249363e04feeebaf3e7f278d60f7d5d49427
elenaacinapura <elena.acinapura@gmail.com>
2021-04-21 16:45:09 +0200

    execute numerov

--- include/differential_eq/numerov.h
+++ include/differential_eq/numerov.h
@@ -20,4 +20,17 @@
  */
 double complex numerov_1D (double x, complex double psi_curr, complex double psi_prec, double dx, double F (double, void *), void *param);
 
+/**
+ * @brief Execute the numerov algorithm and fill e vector with the value of the wave function.
+ * 
+ * @param x Array to store the values of the independent variable. The first two elements should be already initialized.
+ * @param psi Array to store the values of the wave function. The first two elements should be already initialized.
+ * @param dim Dimension of the array x and psi.
+ * @param F Function F in the equation psi''(x) = F(x)*psi(x).
+ * @param param Pointer to the struct with the parameters of F.
+ * 
+ * @return
+ */
+void execute_numerov (double x[], complex double psi[], int dim, double dx, double F (double, void *), void *p);
+
 #endif
--- src/differential_eq/numerov.c
+++ src/differential_eq/numerov.c
@@ -3,3 +3,13 @@
 double complex numerov_1D (double x, complex double psi_curr, complex double psi_prec, double dx, double F (double, void *), void *param) {
     return (psi_curr * (2.0 + 5.0/6 * dx*dx * F(x, param)) - psi_prec * (1 - dx*dx / 12.0 * F(x - dx, param)))/(1 - dx*dx/12.0 * F(x + dx, param));
 }
+
+void execute_numerov (double x[], complex double psi[], int dim, double dx, double F (double, void *), void *p) {
+    int i = 2; 
+    while (i < dim) {
+        complex double psi_curr = numerov_1D(x[i-1], psi[i-1], psi[i-2], dx, F, p);
+        psi[i] = psi_curr;
+        x[i] = x[i-1] + dx;
+        i++;
+    }
+}
