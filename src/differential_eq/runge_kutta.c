edfb74b67ced30881f339cf65103ff1237645342
elenaacinapura <elena.acinapura@gmail.com>
2021-04-27 09:06:42 +0200

    nothing

--- src/differential_eq/runge_kutta.c
+++ src/differential_eq/runge_kutta.c
@@ -1,10 +1,10 @@
 #include <differential_eq/runge_kutta.h>
 
-double RK4(double x_curr, double t_curr, double dt, double f(double, double, void *), void *param) {
-    double K1 = f(x_curr, t_curr, param);
-    double K2 = f(x_curr + 0.5*dt*K1, t_curr + 0.5*dt, param);
-    double K3 = f(x_curr + 0.5*dt*K2, t_curr + 0.5*dt, param);
-    double K4 = f(x_curr + dt*K3, t_curr + dt, param);
+double RK4(double x, double t, double dt, double f(double, double, void *), void *param) {
+	double K1 = f(x, t, param);
+	double K2 = f(x + 0.5 * dt * K1, t + 0.5 * dt, param);
+	double K3 = f(x + 0.5 * dt * K2, t + 0.5 * dt, param);
+	double K4 = f(x + dt * K3, t + dt, param);
 
-    return x_curr + dt/6* (K1 + 2*K2 + 2*K3 + K4);
+	return x + dt / 6.0 * (K1 + 2.0 * K2 + 2.0 * K3 + K4);
 }
