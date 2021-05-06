53c65ac192c09c583a3ee2920e1380285ba61d01
elenaacinapura <elena.acinapura@gmail.com>
2021-05-06 11:13:34 +0200

    removed variable I in integrate() that conficts with complex

--- src/numerical_methods/integrate_notypesafe.c
+++ src/numerical_methods/integrate_notypesafe.c
@@ -10,13 +10,13 @@ double integrate(double f(double, void *), double low, double high, int density,
 
 	int n = (int)ceil((high - low) * density); /* number of points */
 	double dx = (high - low) / (n + 1);	   /* increment in integration variable */
-	double I = 0;
+	double integral = 0;
 
 	for (int i = 1; i < n; i++) {
 		double x1 = low + dx * (i);
 		double x2 = x1 + dx;
-		I += f((x1 + x2) / 2.0, param);
+		integral += f((x1 + x2) / 2.0, param);
 	}
 
-	return I *= dx;
+	return integral *= dx;
 }
