57e4f07ecb1bf9e532fbb42676b73874499a5175
elenaacinapura <elena.acinapura@gmail.com>
2021-05-09 15:31:00 +0200

    fft radial inverse

--- include/numerical_methods/fft_radial.h
+++ include/numerical_methods/fft_radial.h
@@ -1,8 +1,30 @@
 #ifndef __FFT_RADIAL_H__
 #define __FFT_RADIAL_H__
 
+/**
+ * @brief Perform the radial fourier transform via FFT
+ * 
+ * Overwrite the function vector with the values of the radial transform.
+ * 
+ * @param f array with dimension 2*N, where the even and odd values are the real and complex parts of the function to be transformed.
+ * @param N number of points considered for the transform.
+ * @param R [0, R) is the chosen interval of periodicity for the function f.
+ * 
+ * @return
+ */
 void fft_radial_forward (double f[], int N, double R);
 
+/**
+ * @brief Perform the inverse radial fourier transform via FFT
+ * 
+ * Overwrite the function vector with the values of the inverse radial transform.
+ * 
+ * @param f array with dimension 2*N, where the even and odd values are the real and complex parts of the function to be antitransformed.
+ * @param N number of points considered for the antitransform.
+ * @param R [0, R) is the chosen interval of periodicity for the function f.
+ * 
+ * @return
+ */
 void fft_radial_inverse (double f[], int N, double R);
 
 
--- src/numerical_methods/fft_radial.c
+++ src/numerical_methods/fft_radial.c
@@ -14,15 +14,48 @@ void fft_radial_forward (double f[], int N, double R) {
         f[2*n] *= r;
         f[2*n + 1] *= r;
     }
+
     /* Copy f to temp vector */
     double tmp [2*N];
     vec_copy(2*N, f, tmp);
+
     /* Transform */
     gsl_fft_complex_radix2_forward(tmp, 1, N);
+
     /* Take imaginary value and divide by K = pi*k/R */
     for (int k = 1; k < N; k++) {
         f[2*k] =  -R/(M_PI * k) * tmp[2*k + 1];
     }  
+
     /* Assign the value for k=0 */ 
     f[0] = f_0;
 }    
+
+void fft_radial_inverse (double f[], int N, double R) {
+    double dr = R/N;
+
+    double f_0 = 0.0;   /* Value of the antitransform in n=0 */
+    /* Multiply by k */
+    for (int i = 0; i < N; i++) {
+        double k = M_PI * i / R;
+        f_0 += 1.0/N * k*k * f[2*i];
+
+        f[2*i] *= k;
+        f[2*i + 1] *= k;
+    }
+
+    /* Copy f to temp vector */
+    double tmp [2*N];
+    vec_copy(2*N, f, tmp);
+
+    /* Transform  (I don't need to use the antitransform here)*/
+    gsl_fft_complex_radix2_forward(tmp, 1, N);
+
+    /* Take imaginary value and divide by r = n*R/N */
+    for (int n = 1; n < N; n++) {
+        f[2*n] =  -1.0/N * tmp[2*n + 1];
+        f[2*n] *= N / (n * R);
+    }  
+    /* Assign the value for n=0 */ 
+    f[0] = f_0;
+}
