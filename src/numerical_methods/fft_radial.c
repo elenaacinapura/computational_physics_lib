faa94f563f5b3ad5a7ceb740aa325be7ea6c5164
elenaacinapura <elena.acinapura@gmail.com>
2021-05-10 12:25:08 +0200

    corrections to fft

--- src/numerical_methods/fft_radial.c
+++ src/numerical_methods/fft_radial.c
@@ -4,20 +4,26 @@
 #include <math.h>
 
 void fft_radial_forward (double f[], int N, double R) {
-    double dr = R/N;
-    double tmp [2*N];
+    double tmp [4*N];
     double f_0 = 0.0;   /* Value of the transform in k=0 */
 
     /* Multiply by r and fill tmp*/
     for (int n = 0; n < N; n++) {
-        double r = n * dr;
+        double r = n * R / N;
         f_0 += 2.0 * r*r * f[n];
         tmp[2*n] = f[n] * r;
         tmp[2*n + 1] = 0.0;
     }
+    tmp[2*N] = 0.0;
+    tmp[2*N + 1] = 0.0;
+    for (int n = N + 1; n < 2*N; n++) {
+        double r = n * R / N;
+        tmp[2*n] = - f[2*N - n] * r;
+        tmp[2*n + 1] = 0.0;
+    }
 
     /* Transform */
-    gsl_fft_complex_radix2_forward(tmp, 1, N);
+    gsl_fft_complex_radix2_forward(tmp, 1, 2*N);
 
     /* Take imaginary value and divide by K = pi*k/R */
     for (int k = 1; k < N; k++) {
@@ -31,7 +37,7 @@ void fft_radial_forward (double f[], int N, double R) {
 void fft_radial_inverse (double f[], int N, double R) {
 
     double f_0 = 0.0;   /* Value of the antitransform in n=0 */
-    double tmp [2*N];
+    double tmp [4*N];
     /* Multiply by k */
     for (int i = 0; i < N; i++) {
         double k = M_PI * (double)(i) / R;
@@ -39,9 +45,16 @@ void fft_radial_inverse (double f[], int N, double R) {
         tmp[2*i] = k * f[i];
         tmp[2*i + 1] = 0.0;
     }
+    tmp[2*N] = 0.0;
+    tmp[2*N + 1] = 0.0;
+    for (int i = N + 1; i < 2*N; i++) {
+        double k = M_PI * (double)(i) / R;
+        tmp[2*i] = - f[2*N - i] * k;
+        tmp[2*i + 1] = 0.0;
+    }
 
     /* Transform  (I don't need to use the antitransform here)*/
-    gsl_fft_complex_radix2_forward(tmp, 1, N);
+    gsl_fft_complex_radix2_forward(tmp, 1, 2*N);
 
     /* Take imaginary value and divide by r = n*R/N */
     for (int n = 1; n < N; n++) {
