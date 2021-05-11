4005901891a5aed51c58c1295f7a0bc64831e111
elenaacinapura <elena.acinapura@gmail.com>
2021-05-11 10:23:54 +0200

    var nenaming

--- src/numerical_methods/fft_radial.c
+++ src/numerical_methods/fft_radial.c
@@ -1,37 +1,37 @@
-#include <numerical_methods/fft_radial.h>
 #include <gsl/gsl_fft_complex.h>
 #include <linear_algebra/blas_wrappers.h>
 #include <math.h>
+#include <numerical_methods/fft_radial.h>
 
 void fft_radial_forward(double f[], int N, double R) {
 	int M = 2 * N;
-	double tmp[2 * M];
+	double C_bar[2 * M];
 	double f_0 = 0.0; /* Value of the transform in k=0 */
 
-	/* Multiply by r and fill tmp*/
+	/* Multiply by r and fill C_bar*/
 	for (int n = 0; n < M; n++) {
 		if (n < N) {
 			double r = n * R / N;
 			f_0 += 2.0 * r * r * f[n];
-			tmp[2 * n] = f[n] * r;
-			tmp[2 * n + 1] = 0.0;
+			C_bar[2 * n] = f[n] * r;
+			C_bar[2 * n + 1] = 0.0;
 		}
 		if (n == N) {
-			tmp[2 * n] = 0.0;
-			tmp[2 * n + 1] = 0.0;
+			C_bar[2 * n] = 0.0;
+			C_bar[2 * n + 1] = 0.0;
 		}
 		if (n > N) {
-			tmp[2 * n] = -tmp[2*(2*N - n)];
-			tmp[2 * n + 1] = 0.0;
+			C_bar[2 * n] = -C_bar[2 * (2 * N - n)];
+			C_bar[2 * n + 1] = 0.0;
 		}
 	}
 
 	/* Transform */
-	gsl_fft_complex_radix2_forward(tmp, 1, M);
+	gsl_fft_complex_radix2_forward(C_bar, 1, M);
 
 	/* Take imaginary value and divide by K = pi*k/R */
 	for (int k = 1; k < N; k++) {
-		f[k] = -R / (M_PI * k) * tmp[2 * k + 1];
+		f[k] = -R / (M_PI * k) * C_bar[2 * k + 1];
 	}
 
 	/* Assign the value for k=0 */
@@ -40,33 +40,33 @@ void fft_radial_forward(double f[], int N, double R) {
 
 void fft_radial_inverse(double f[], int N, double R) {
 	int M = 2 * N;
-	double tmp[2 * M];
+	double C_bar[2 * M];
 	double f_0 = 0.0; /* Value of the antitransform in n=0 */
 
-	/* Multiply by k and fill tmp*/
+	/* Multiply by k and fill C_bar*/
 	for (int i = 0; i < M; i++) {
 		if (i < N) {
 			double k = M_PI * (double)(i) / R;
 			f_0 += 1.0 / N * k * k * f[i];
-			tmp[2 * i] = k * f[i];
-			tmp[2 * i + 1] = 0.0;
+			C_bar[2 * i] = k * f[i];
+			C_bar[2 * i + 1] = 0.0;
 		}
 		if (i == 0) {
-			tmp[2 * i] = 0.0;
-			tmp[2 * i + 1] = 0.0;
+			C_bar[2 * i] = 0.0;
+			C_bar[2 * i + 1] = 0.0;
 		}
 		if (i > N) {
-			tmp[2 * i] = -tmp[2*(2 * N - i)];
-			tmp[2 * i + 1] = 0.0;
+			C_bar[2 * i] = -C_bar[2 * (2 * N - i)];
+			C_bar[2 * i + 1] = 0.0;
 		}
 	}
 
 	/* Transform  (I don't need to use the antitransform here) */
-	gsl_fft_complex_radix2_forward(tmp, 1, M);
+	gsl_fft_complex_radix2_forward(C_bar, 1, M);
 
 	/* Take imaginary value and divide by r = n*R/N */
 	for (int n = 1; n < N; n++) {
-		f[n] = -1.0 / M * tmp[2 * n + 1];
+		f[n] = -1.0 / M * C_bar[2 * n + 1];
 		f[n] *= (double)(N) / (n * R);
 	}
 	/* Assign the value for n=0 */
